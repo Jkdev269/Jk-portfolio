766926a5023f086b394e317f29c0dae63a020d7c
Jkdev269 <jaharuddin269@gmail.com>
2025-04-13 16:40:37 +0530

    update project url

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,44 +1,37 @@
-import project1_img from '../assets/project_1.svg'
-import project2_img from '../assets/project_2.svg'
-import project3_img from '../assets/project_3.svg'
-import project4_img from '../assets/project_4.svg'
-import project5_img from '../assets/project_5.svg'
-import project6_img from '../assets/project_6.svg'
+import project1_img from "../assets/project_1.svg";
+import project2_img from "../assets/project_2.svg";
+import project3_img from "../assets/project_3.png";
+import project4_img from "../assets/project_4.png";
+import project5_img from "../assets/project_5.svg";
+import project6_img from "../assets/project_6.svg";
 
 const mywork_data = [
-    {
-        w_no:1,
-        w_name:"Web design",
-        w_img:project1_img,
-        project_url:"https://jobportal-43m2.onrender.com"
-    },
-    {
-        w_no:2,
-        w_name:"Web design",
-        w_img:project2_img,
-        project_url:"https://nexamart-jk.vercel.app/"
-    },
-    {
-        w_no:3,
-        w_name:"Web design",
-        w_img:project3_img,
-        project_url:"https://mini-chating-application.onrender.com"
-    },
-    // {
-    //     w_no:4,
-    //     w_name:"Web design",
-    //     w_img:project4_img
-    // },
-    // {
-    //     w_no:5,
-    //     w_name:"Web design",
-    //     w_img:project5_img
-    // },
-    // {
-    //     w_no:6,
-    //     w_name:"Web design",
-    //     w_img:project6_img
-    // },
-]
+  {
+    w_no: 4,
+    w_name: "Web design",
+    w_img: project4_img,
+    project_url: "https://ddoaz7cm594yq.cloudfront.net/Jtext",
+  },
+  {
+    w_no: 3,
+    w_name: "Web design",
+    w_img: project3_img,
+    project_url: "https://feedback-inbox.vercel.app/",
+  },
+  {
+    w_no: 2,
+    w_name: "Web design",
+    w_img: project2_img,
+    project_url: "https://nexamart-jk.vercel.app/",
+  },
  
+
+  {
+    w_no: 1,
+    w_name: "Web design",
+    w_img: project1_img,
+    project_url: "https://jobportal-43m2.onrender.com",
+  },
+];
+
 export default mywork_data;
